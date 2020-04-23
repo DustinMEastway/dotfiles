f0e0d45009ce3eb1f0b7887ac2303251c99fd96b
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2020-04-22 23:42:25 -0400

    add git configuration file

--- functions/logging.sh
+++ functions/logging.sh
@@ -2,20 +2,20 @@
 #
 # Logging functions to display messages to the user
 
-logFail() {
+function logFail() {
 	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
 	echo ''
 	exit
 }
 
-logInfo() {
+function logInfo() {
 	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
-logQuestion() {
+function logQuestion() {
 	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
 }
 
-logSuccess() {
+function logSuccess() {
 	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
