2ee523eb50e4b570c136dfc5d0d4e4fccf0b054b
Dustin M. Eastway <deastway@interactions.com>
2024-01-18 11:12:16 -0500

    update work config

--- environments/DustinMEastway/work-macos/sh/aliases.symlink.sh
+++ environments/DustinMEastway/work-macos/sh/aliases.symlink.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+alias dockerCleanup='sudo docker system prune -a -f && docker volume rm $(docker volume ls -qf dangling=true)'
+alias npmI='npm install --legacy-peer-deps'
+alias npmU='npm uninstall --legacy-peer-deps'
+alias sshDev='ssh deastway@dev.d2.interactions.net'
+alias sshUat1='ssh deastway@uat1.d2.interactions.net'
+alias sshUat2='ssh deastway@uat2.d2.interactions.net'
