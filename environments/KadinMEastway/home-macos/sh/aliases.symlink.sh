bd75a565766a8d2b478a2a19d6d7025b0f2e4a56
KadinMEastway <Kadin.eastway2021@gmail.com>
2024-01-31 17:54:28 -0500

    added new functions to launch brew services

--- environments/KadinMEastway/home-macos/sh/aliases.symlink.sh
+++ environments/KadinMEastway/home-macos/sh/aliases.symlink.sh
@@ -5,8 +5,32 @@ alias calibredb='/Applications/calibre.app/Contents/MacOS/calibredb'
 alias cdCalibre='cd ~/Documents/Books/Calibre\ Library/'
 alias openICloud='open ~/Library/Mobile\ Documents/com~apple~CloudDocs/'
 
-# *** Mongodb aliases *** #
+
+# *** Brew service functions *** #
+function service() {
+  case "$1" in
+    start)
+      brew services start $2
+      ;;
+    stop)
+      brew services stop $2
+      ;;
+    restart)
+      brew services restart $2
+      ;;
+    *)
+      echo "Usage: {start|stop|restart}"
+      ;;
+  esac
+}
+
+# *** PostgreSQL *** #
+function pg() {
+  service $1 postgresql
+}
+
+# *** MongoDB *** #
+function mg() {
+  service $1 mongodb-community
+}
 alias mongoConfig='function _() { edit $(brew --prefix)/etc/mongod.conf }; _'
-alias mongoRestart='brew services restart mongodb-community'
-alias mongoStart='brew services start mongodb-community'
-alias mongoStop='brew services stop mongodb-community'
