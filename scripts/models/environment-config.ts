ed3b1ce1c66b5d786d80c60a46047d5c3206409e
Alex Flynn <aflynn501@gmail.com>
2024-04-18 19:25:07 -0400

    added config path to environment config

--- scripts/models/environment-config.ts
+++ scripts/models/environment-config.ts
@@ -1,7 +1,11 @@
 import { CommandConfig } from './command-config';
 
-/** configuration used to set up an environment */
+/** Configuration used to set up an environment. */
 export type EnvironmentConfig = {
-  fileRoot: string;
+  /** The commands to run. */
   commands: CommandConfig[];
+  /** The path to the configuration file. */
+  configPath: string;
+  /** The root directory of the configuration file. */
+  fileRoot: string;
 };
