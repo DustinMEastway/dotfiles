920473df2510911e882bfbc63102d60cdf7567a6
Alex Flynn <aflynn501@gmail.com>
2024-05-20 20:58:48 -0400

    Updated brew-sync with comment recommendations

--- scripts/commands/brew-sync.ts
+++ scripts/commands/brew-sync.ts
@@ -6,10 +6,14 @@ import {
   readJsonFile,
   writeJsonFile
 } from '../functions/index';
-import { Command, CommandConfig, EnvironmentConfig } from '../models';
+import {
+  Command,
+  CommandKey,
+  EnvironmentConfig
+} from '../models';
 
 /**
- * Synchronizes Brew items in `congig.json` with the items installed on your machine.
+ * Syncs the Brew items in the configuration file with the installed Brew items.
  * 
  * @param config - The configuration object containing the path to the config file.
  */
@@ -17,30 +21,17 @@ export const brewSync: Command = async (config) => {
   logInfo('BrewSync process initiated...');
   try {
     const data = await readJsonFile(config.configPath) as EnvironmentConfig;
-    const { casks, formulae } = await getUpdatedBrewItems(data) ?? { casks: [], formulae: [] };
+    const isCasksUpdated = await updateBrewItems(data, true);
+    const isFormulaeUpdated = await updateBrewItems(data, false);
 
-    // TODO - Better way to handle this?
-    if (!casks.length && !formulae.length) {
+    if (!isCasksUpdated && !isFormulaeUpdated) {
+      logSuccess('No new items to sync.');
       return;
-    }
-    if (casks.length) {
-      data.commands.forEach((command: CommandConfig) => {
-        if (command.key === 'brew-install' && command.value.args === '--cask') {
-          command.value.items = casks;
-        }
-      });
-    }
-    if (formulae.length) {
-      data.commands.forEach((command: CommandConfig) => {
-        if (command.key === 'brew-install' && !command.value.args) {
-          command.value.items = formulae;
-        }
-      });
     }
 
     logInfo('New items detected, Syncronization in 3... 2... 1... 🚀');
-    await writeJsonFile(config.configPath, data)
-      .then(() => logSuccess('Brew items syncronized!'));
+    await writeJsonFile(config.configPath, data);
+    logSuccess('Brew items syncronized!');
   }
   catch (error) {
     logFail(`Error syncing brew items: ${error}`);
@@ -48,58 +39,70 @@ export const brewSync: Command = async (config) => {
 }
 
 /**
- * Gets any new brew items that are not already in `config.json`.
- * 
- * @param data - The configuration object containing the path to the config file.
- * @returns  The new Brew items to be added to the configuration.
- * @throws An error if the Brew items cannot be found.
- */
-const getUpdatedBrewItems = async (data: EnvironmentConfig) => {
-  try {
-    logInfo('Checking for new casks...');
-    const newCasks = await filterBrew(data, true);
-
-    logInfo('Checking for new formulae...');
-    const newFormulae = await filterBrew(data, false);
-
-    if (!newCasks.length && !newFormulae.length) {
-      logSuccess('No new items to sync.');
-      return;
-    }
-
-    const jsonCasks = data.commands.find((command: CommandConfig) => {
-      return command.key === 'brew-install' && command.value?.args === '--cask'
-    })?.value.items as string[];
-
-    const jsonFormulae = data.commands.find((command: CommandConfig) => {
-      return command.key === 'brew-install' && !command.value?.args
-    })?.value.items as string[];
-
-    const casks = [...jsonCasks, ...newCasks].sort();
-    const formulae = [...jsonFormulae, ...newFormulae].sort();
-
-    return { casks, formulae };
-  }
-  catch (error) {
-    logFail(`Error finding brew items: ${error}`);
-  }
-}
-
-/**
- * Filters the Brew items based on the provided configuration.
+ * Updates the Brew items in the configuration file.
  * 
  * @param data - The configuration object containing the path to the config file.
  * @param isCask - A boolean indicating whether the Brew item is a cask.
- * @returns  A promise that resolves with the filtered Brew items.
+ * @returns  A promise that resolves with a boolean indicating whether the Brew items were updated.
  */
-const filterBrew = async (data: EnvironmentConfig, isCask: boolean) => {
-  return (await exec(isCask ? 'brew list --cask' : 'brew list --formula'))
-    .split('\n')
-    .filter(line => line.trim() !== '')
-    .filter((item: string) => !data.commands
-      .find((command: CommandConfig) => {
-        return command.key === 'brew-install'
-          && (isCask ? command.value?.args === '--cask' : !command.value?.args)
-          && command.value.items.includes(item)
-      }));
+async function updateBrewItems(data: EnvironmentConfig, isCask: boolean): Promise<boolean> {
+  const type = isCask ? 'casks' : 'formulae';
+  try {
+    logInfo(`Checking for new ${type}...`);
+
+    // Check to see if the brew-install commands exist.
+    const commands = data.commands.filter((command) => {
+      return (
+        command.key === 'brew-install'
+        && command.value
+        && isCask === /--casks?\b/.test(command.value.args ?? '')
+      )
+    });
+
+    // If commands do not exist, create them.
+    if (!commands.length) {
+      const newCommand = {
+        key: CommandKey.brewInstall,
+        value: {
+          args: (isCask) ? '--cask' : '',
+          items: []
+        }
+      };
+      data.commands.push(newCommand);
+      commands.push(newCommand);
+    }
+
+    // Grab the list of items from the commands.
+    const commandItems = new Set<string>(commands.map((command) => {
+      return command.value?.items ?? [];
+    }).flat());
+
+    // Get the list of installed items.
+    const brewListCommand = `brew ${(isCask) ? 'list --cask' : 'leaves'}`;
+    const installedItems = (await exec(brewListCommand)).split('\n');
+
+    // Find the items that are in the installed list but not in the command list.
+    const missingItems = installedItems.map((item) => {
+      return item.trim();
+    }).filter((item) => {
+      return (
+        item
+        && !commandItems.has(item)
+      );
+    });
+
+    if (!missingItems.length) {
+      return false;
+    }
+
+    // Add the missing items to the first command.
+    const firstCommand = commands[0];
+    firstCommand.value.items = [...firstCommand.value.items, ...missingItems].sort();
+
+    return true;
+  } catch (error) {
+    logFail(`Error checking for new ${type}: ${error}`);
+  }
+
+  return true;
 }
