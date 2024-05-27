fccece4ea982bfc4cd119bbdd3dc03dd625ca482
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2024-05-27 16:12:52 -0400

    add 1password to DustinMEastway

---
+++ environments/DustinMEastway/common/sh/bin/openInsomnia.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Insomnia API
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description Open Insomnia with my environment variables present.
+# @raycast.author Dustin M. Eastway
+
+source ~/.bashrc
+open /Applications/Insomnia.app
