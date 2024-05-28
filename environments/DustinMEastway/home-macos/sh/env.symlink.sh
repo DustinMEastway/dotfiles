195ba82ebaafbf8b31f4d6300f774242627a30b5
Dustin M. Eastway <deastway@interactions.com>
2024-05-28 14:49:43 -0400

    add 1Password to DustinMEastway work config

--- environments/DustinMEastway/home-macos/sh/env.symlink.sh
+++ environments/DustinMEastway/home-macos/sh/env.symlink.sh
@@ -5,10 +5,10 @@ export DOTFILES_CONFIG='./environments/DustinMEastway/home-macos/config.json'
 export DOTNET_ROOT='/opt/homebrew/bin'
 export NOTES_BACKUP_PATH='/Users/deastway/Sites/notes/'
 export NOTES_PATH='/Users/deastway/Library/Mobile Documents/iCloud~md~obsidian/Documents/second-brain/'
-export OMDB_TOKEN="op://Private/omdb/token";
-export TWITCH_CLIENT_ID="op://Private/twitch/clientId";
-export TWITCH_CLIENT_SECRET="op://Private/twitch/clientSecret";
-export YOUTUBE_TOKEN="op://Private/googlePersonal/youtubeToken";
+export OMDB_TOKEN='op://Private/omdb/token';
+export TWITCH_CLIENT_ID='op://Private/twitch/clientId';
+export TWITCH_CLIENT_SECRET='op://Private/twitch/clientSecret';
+export YOUTUBE_TOKEN='op://Private/googlePersonal/youtubeToken';
 
 export PATH="$PATH:$ANDROID_SDK_ROOT/emulator";
 export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools";
--- environments/DustinMEastway/work-macos/sh/env.symlink.sh
+++ environments/DustinMEastway/work-macos/sh/env.symlink.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# System.
 export DOTFILES_CONFIG='./environments/DustinMEastway/work-macos/config.json'
 export NOTES_BACKUP_PATH='/Users/deastway/Sites/notes/'
 export NOTES_PATH='/Users/deastway/Library/Mobile Documents/iCloud~md~obsidian/Documents/notes/'
+
+# Login.
+export JIRA_AUTH_TOKEN='op://Private/workLdap/jiraToken'
+export KEYCLOAK_PASSWORD='op://Private/workLdap/password'
+export KEYCLOAK_USERNAME='op://Private/workLdap/username'
+export MAC_ADDRESS='op://Private/workComputer/macAddress'
+export NPM_AUTH_TOKEN='op://Private/workLdap/npmToken'
+
+# HXP Micro Frontends.
+export HXP_CONFIG_REMOTE='http://localhost:3010'
+export HXP_CONTAINER_REMOTE='http://localhost:3000'
+export REACT_APP_API_URL='http://localhost:3000/api'
