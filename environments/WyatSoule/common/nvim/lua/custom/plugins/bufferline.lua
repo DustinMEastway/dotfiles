d5f9bf650a12a7dc838f236898432ab60268068b
Wyat Soule <wsoule679@gmail.com>
2024-06-10 20:34:31 -0400

    delete old nvim config & update some plugins on wyat nvim

---
+++ environments/WyatSoule/common/nvim/lua/custom/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require('bufferline').setup {
+      options = {
+        diagnostics = 'nvim_lsp',
+      },
+    }
+
+    vim.keymap.set('n', '<C-S-[>', ':bprevious<CR>', { silent = true })
+    vim.keymap.set('n', '<C-S-]>', ':bNext<CR>', { silent = true })
+    vim.keymap.set('n', '<C-S-T>', ':e #<CR>', { silent = true })
+  end,
+}
