cdba332ebab8061b1c7fdc9f407687147a34918a
Wyat Soule <wsoule679@gmail.com>
2024-12-07 16:30:00 -0500

    feat: update wyat zshrc to have new config

--- environments/WyatSoule/common/sh/zshrc.symlink.zsh
+++ environments/WyatSoule/common/sh/zshrc.symlink.zsh
@@ -1,8 +1,91 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # Run common commands.
 if [[ -f "$HOME/.common.sh" ]]
 then
   source "$HOME/.common.sh"
 fi
+
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# # Add deno completions to search path
+# if [[ ":$FPATH:" != *":/Users/waggoner-wyat/.zsh/completions:"* ]]; then export FPATH="/Users/waggoner-wyat/.zsh/completions:$FPATH"; fi
+# Run common commands.
+# if [[ -f "$HOME/.common.sh" ]]
+# then
+#   source "$HOME/.common.sh"
+# fi
+#
+
+
+
+# Set the directory we want to store zinit and plugins
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+
+# Download Zinit, if it's not there yet
+if [ ! -d "$ZINIT_HOME" ]; then
+   mkdir -p "$(dirname $ZINIT_HOME)"
+   git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+# Source/Load zinit
+source "${ZINIT_HOME}/zinit.zsh"
+#
+# Add in Powerlevel10k
+zinit ice depth=1; zinit light romkatv/powerlevel10k
+
+# add in zsh plugins
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
+zinit light aloxaf/fzf-tab
+
+# Add in snippets
+zinit snippet OMZP::git
+
+# Load completions
+autoload -U compinit && compinit
+
+zinit cdreplay -q
+
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# History
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=$HISTSIZE
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
+
+# Completion styling
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+# zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+# zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+# Aliases
+alias ls='ls --color'
+
+# Shell integrations
+eval "$(fzf --zsh)"
+eval "$(zoxide init --cmd cd zsh)"
+
 
 # bun completions
 [ -s "/Users/wsoule/.bun/_bun" ] && source "/Users/wsoule/.bun/_bun"
@@ -23,3 +106,5 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+
