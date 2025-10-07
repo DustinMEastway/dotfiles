026fdb762bf1a4a46d130e80d7feb6a50f6ac1b9
Wyat Soule <wsoule679@gmail.com>
2025-10-06 22:48:05 -0400

    dadbod

--- environments/WyatSoule/common/nvim/lua/custom/plugins/db.lua
+++ environments/WyatSoule/common/nvim/lua/custom/plugins/db.lua
@@ -1,5 +1,16 @@
 return {
-  'tpope/vim-dadbod',
-  'kristijanhusak/vim-dadbod-completion',
-  'kristijanhusak/vim-dadbod-ui',
+  {
+    'tpope/vim-dadbod',
+    dependencies = {
+      'kristijanhusak/vim-dadbod-completion',
+      'kristijanhusak/vim-dadbod-ui',
+    },
+    config = function()
+      -- Dadbod UI keymaps
+      vim.keymap.set('n', '<leader>du', ':DBUIToggle<CR>', { silent = true, desc = '[D]B [U]I toggle' })
+      vim.keymap.set('n', '<leader>df', ':DBUIFindBuffer<CR>', { silent = true, desc = '[D]B [F]ind buffer' })
+      vim.keymap.set('n', '<leader>dr', ':DBUIRenameBuffer<CR>', { silent = true, desc = '[D]B [R]ename buffer' })
+      vim.keymap.set('n', '<leader>dq', ':DBUILastQueryInfo<CR>', { silent = true, desc = '[D]B last [Q]uery info' })
+    end,
+  },
 }
